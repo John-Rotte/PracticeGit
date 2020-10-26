b449cfc96da84c0afcefe50961a7046202d28b07
John-Rotte <john.roth_95@live.com>
2020-10-26 14:27:37 +0100

    First Entry

---
+++ src/se/lektioner/Dag1.java
@@ -0,0 +1,14 @@
+package se.lektioner;
+
+public class Dag1 {
+    //Lägg upp allt du kodar på git och lägg till din git repocitory på ditt CV.
+    //Försök att implementera användning av branches när du lägger till nya funktioner på dina projekt
+
+    static String face = "d(O,O)b";
+
+    public static void main(String[] args) {
+
+        System.out.println("Look at this beautiful face \t" + face);
+
+    }
+}
